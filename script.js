48417aed4b37b00062b48a8dc66860500c39cadf
Niro7 <33750983+Niro7@users.noreply.github.com>
2020-07-05 19:30:18 +0530

    UPDATE: Hover, active state styles

--- script.js
+++ script.js
@@ -1,127 +1,118 @@
-
-
 window.onload = () => {
-    getCountryData();
-    getHistoricalData();
-}
-
+  getCountryData();
+  getHistoricalData();
+  getWorldCoronaData();
+  // document.querySelector(".active-cases-card").addEventListener("click", () => {
+  //   console.log("Yooo we clicked!!!"); //Not Working
+  // });
+};
 
 var map;
 var infoWindow;
+let coronaGlobalData;
+let mapCircles = [];
+var casesTypeColors = {
+  cases: "#1d2c4d",
+  active: "#9d80fe",
+  recovered: "#7dd71d",
+  deaths: "#fb4443",
+};
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 39.8283, lng: -98.5795},
-        zoom: 3,
-        styles: mapStyle
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 39.8283, lng: -98.5795 },
+    zoom: 3,
+    styles: mapStyle,
+  });
+  infoWindow = new google.maps.InfoWindow();
+}
+const changeDataSelection = (casesType) => {
+  // console.log(mapCircles);
+  clearTheMap(); //Clear the existing Markers
+  showDataOnMap(coronaGlobalData, casesType);
+
+  // var header = document.getElementById("cards-container");
+  var btns = document.getElementsByClassName("stat-card-container");
+  // // console.log(btns);
+  // header.forEach((card) => {
+  //   console.log(card);
+  // });
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+      var current = document.getElementsByClassName("cases-card");
+      current[0].className = current[0].className.replace(" cases-card", "");
+      this.className += " cases-card";
     });
-    infoWindow = new google.maps.InfoWindow();
-}
+    // console.log(btns);
+  }
+};
 
-const getCountryData = () => {
-    fetch("https://corona.lmao.ninja/v2/countries")
-    .then((response)=>{
-        return response.json()
-    }).then((data)=>{
-        showDataOnMap(data);
-        showDataInTable(data);
+const clearTheMap = () => {
+  for (let circle of mapCircles) {
+    circle.setMap(null);
+  }
+};
+const getCountryData = (casesType) => {
+  fetch("https://corona.lmao.ninja/v2/countries")
+    .then((response) => {
+      return response.json();
     })
-}
+    .then((data) => {
+      coronaGlobalData = data;
+      // console.log(data);
 
+      showDataOnMap(data);
+      showDataInTable(data);
+    });
+};
+const getWorldCoronaData = () => {
+  fetch("https://disease.sh/v2/all")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      buildPieChart(data);
+    });
+};
 const getHistoricalData = () => {
-    fetch("https://corona.lmao.ninja/v2/historical/all?lastdays=120")
-    .then((response)=>{
-        return response.json()
-    }).then((data)=>{
-        let chartData = buildChartData(data);
-        buildChart(chartData);
+  fetch("https://corona.lmao.ninja/v2/historical/all?lastdays=120")
+    .then((response) => {
+      return response.json();
     })
-}
-
-const buildChartData = (data) => {
-    let chartData = [];
-    for(let date in data.cases){
-        let newDataPoint = {
-            x: date,
-            y: data.cases[date]
-        }
-        chartData.push(newDataPoint);
-    }
-    return chartData;
-}
-
-
-const buildChart = (chartData) => {
-    console.log("All if good");
-    var timeFormat = 'MM/DD/YY';
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'line',
-
-        // The data for our dataset
-        data: {
-            datasets: [{
-                label: 'Total Cases',
-                backgroundColor: '#1d2c4d',
-                borderColor: '#1d2c4d',
-                data: chartData
-            }]
-        },
-
-        // Configuration options go here
-        options: {
-            maintainAspectRatio: false,
-            tooltips: {
-                mode: 'index',
-                intersect: false
-            },
-            scales:     {
-                xAxes: [{
-                    type: "time",
-                    time: {
-                        format: timeFormat,
-                        tooltipFormat: 'll'
-                    }
-                }],
-                yAxes: [{
-                    ticks: {
-                        // Include a dollar sign in the ticks
-                        callback: function(value, index, values) {
-                            return numeral(value).format('0,0');
-                        }
-                    }
-                }]
-            }
-        }
+    .then((data) => {
+      let chartData = buildChartData(data);
+      buildChart(chartData);
     });
-}
-
-
-
+};
 
 const openInfoWindow = () => {
-    infoWindow.open(map);
-}
+  infoWindow.open(map);
+};
 
-const showDataOnMap = (data) => {
-    data.map((country)=>{
-        let countryCenter = {
-            lat: country.countryInfo.lat,
-            lng: country.countryInfo.long
-        }
+const showDataOnMap = (data, casesType = "cases") => {
+  // if (!casesType) {
+  //   casesType = "cases"; //Set Default parameter inside parameter
+  // }
+  data.map((country) => {
+    let countryCenter = {
+      lat: country.countryInfo.lat,
+      lng: country.countryInfo.long,
+    };
+    // console.log(casesTypeColors["casesType"]);
+    // console.log(casesTypeColors.casesType); //Same as above
+    // console.log(casesTypeColors[casesType]);
 
-        var countryCircle = new google.maps.Circle({
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: '#FF0000',
-            fillOpacity: 0.35,
-            map: map,
-            center: countryCenter,
-            radius: country.cases
-        });
-
-        var html = `
+    var countryCircle = new google.maps.Circle({
+      strokeColor: casesTypeColors[casesType],
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: casesTypeColors[casesType],
+      fillOpacity: 0.35,
+      map: map,
+      center: countryCenter,
+      radius: country[casesType],
+    });
+    mapCircles.push(countryCircle);
+    var html = `
             <div class="info-container">
                 <div class="info-flag" style="background-image: url(${country.countryInfo.flag});">
                 </div>
@@ -138,36 +129,36 @@ const showDataOnMap = (data) => {
                     Deaths: ${country.deaths}
                 </div>
             </div>
-        `
+        `;
 
-        var infoWindow = new google.maps.InfoWindow({
-            content: html,
-            position: countryCircle.center
-        });
-        google.maps.event.addListener(countryCircle, 'mouseover', function() {
-            infoWindow.open(map);
-        });
+    var infoWindow = new google.maps.InfoWindow({
+      content: html,
+      position: countryCircle.center,
+    });
+    google.maps.event.addListener(countryCircle, "mouseover", function () {
+      infoWindow.open(map);
+    });
 
-        google.maps.event.addListener(countryCircle, 'mouseout', function(){
-            infoWindow.close();
-        })
-
-    })
-
-}
+    google.maps.event.addListener(countryCircle, "mouseout", function () {
+      infoWindow.close();
+    });
+  });
+};
 
 const showDataInTable = (data) => {
-    var html = '';
-    data.forEach((country)=>{
-        html += `
+  var html = "";
+  data.forEach((country) => {
+    html += `
         <tr>
-            <td>${country.country}</td>
+            <td>
+    <img src="${country.countryInfo.flag}" alt="Flag">
+            
+            ${country.country}</td>
             <td>${country.cases}</td>
             <td>${country.recovered}</td>
             <td>${country.deaths}</td>
         </tr>
-        `
-    })
-    document.getElementById('table-data').innerHTML = html;
-}
-
+        `;
+  });
+  document.getElementById("table-data").innerHTML = html;
+};
